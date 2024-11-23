3432764bfd214ede9cdb28c266f255d8e4c71964
matteocao <38946719+matteocao@users.noreply.github.com>
2024-11-23 12:00:00 +0100

    linting

--- giotto_llm/prompts/consts.py
+++ giotto_llm/prompts/consts.py
@@ -1,8 +1,4 @@
-from giotto_llm.prompts.text_prompts import (
-    PromptSolveInstrV1,
-    PromptSolveInstrV2,
-    PromptSolveShort,
-)
+from giotto_llm.prompts.text_prompts import PromptSolveInstrV1, PromptSolveInstrV2, PromptSolveShort
 
 TYPES_OF_PROMPTS = {
     "prompt_solve_instr_v1": PromptSolveInstrV1,
